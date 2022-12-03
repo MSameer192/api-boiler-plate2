9dd4f90e27d3233c89adc1c6ae735b777f331bf6
MSameer192 <112206736+MSameer192@users.noreply.github.com>
2022-12-03 22:10:56 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ToDOMaster from './components/ToDOMaster';
+// import ToDO from './components/ToDO';
+
+function App() {
+  return (
+    <>
+      <ToDOMaster />
+    </>
+  );
+}
+
+export default App;
